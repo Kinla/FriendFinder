f570d87aedc0e0a5306410d047eebba6e286efe8
SharonC <sharon.s.c.chien@gmail.com>
2019-04-15 00:35:00 -0400

    fixed static file serving issue

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,3 +1,4 @@
 //init chosen
 $(".chosen-select").chosen({disable_search_threshold: 10})
 
+console.log("test")
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,13 @@
 //* A GET Route to `/survey` which should display the survey page.
 //* A default, catch-all route that leads to `home.html` which displays the home page.
-let html = () => {
-
+let html = (app, path) => {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+      
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
 }
 
 module.exports = html
