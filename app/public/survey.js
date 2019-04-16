ea6591ab484c73b684750a3af29de5c7a3a32692
SharonC <sharon.s.c.chien@gmail.com>
2019-04-16 01:59:26 -0400

    validation completed other than image url

--- app/public/survey.js
+++ app/public/survey.js
@@ -13,42 +13,40 @@ $("#submit").on("click", (event) => {
     let surveyResults = {
         name: $("#name").val().trim(),
         photo: $("#photo").val().trim(),
-        q1: parseInt($("#q1").val().trim()),
-        q2: parseInt($("#q2").val().trim()),
-        q3: parseInt($("#q3").val().trim()),
-        q4: parseInt($("#q4").val().trim()),
-        q5: parseInt($("#q5").val().trim()),
-        q6: parseInt($("#q6").val().trim()),
-        q7: parseInt($("#q7").val().trim()),
-        q8: parseInt($("#q8").val().trim()),
-        q9: parseInt($("#q9").val().trim()),
-        q10: parseInt($("#q10").val().trim())
+        q1: $("#q1").val().trim(),
+        q2: $("#q2").val().trim(),
+        q3: $("#q3").val().trim(),
+        q4: $("#q4").val().trim(),
+        q5: $("#q5").val().trim(),
+        q6: $("#q6").val().trim(),
+        q7: $("#q7").val().trim(),
+        q8: $("#q8").val().trim(),
+        q9: $("#q9").val().trim(),
+        q10: $("#q10").val().trim()
     };
         
     // Calculate sum of results
-    surveyResults.sum = surveyResults.q1 + surveyResults.q2 + surveyResults.q3 + surveyResults.q4 + surveyResults.q5 + surveyResults.q6 + surveyResults.q7 + surveyResults.q8 + surveyResults.q9 + surveyResults.q10
+    surveyResults.sum =  parseInt(surveyResults.q1) +  parseInt(surveyResults.q2) +  parseInt(surveyResults.q3) +  parseInt(surveyResults.q4) +  parseInt(surveyResults.q5) +  parseInt(surveyResults.q6) +  parseInt(surveyResults.q7) +  parseInt(surveyResults.q8) +  parseInt(surveyResults.q9) +  parseInt(surveyResults.q10)
 
     // Validation
     let isValid = true
 
-    if (!surveyResults.name) {
-        isValid = false
-    }
+    // This checks all form value to not be ""
+    let values = Object.values(surveyResults)
+
+    for (var i = 0; i < values.length; i++)
+        if(!values[i]){
+            isValid = false
+        }
     
+    // This checs if url leads to actual displayable image --- THIS DOESN'T WORK    
     const imgExists = (url) => {
         let img = new Image();
-        img.url = url
         img.onerror = () => {isValid = false}
+        img.url = url
     }
 
     imgExists(surveyResults.photo)
-
-    for (var i = 1; i < 11; i++){
-        console.log(surveyResults["q" + i])
-        if((surveyResults["q" + i]) === NaN){
-            isValid = false
-        }
-    }
 
     console.log(isValid)
 
@@ -78,7 +76,7 @@ $("#submit").on("click", (event) => {
         });
 
     } else {
-        alert("Please complete every filed of the form.")
+        alert("Please complete every filed of the form.")//should write a modal for this
     }
 
 
